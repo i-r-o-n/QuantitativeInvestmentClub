bee94ce6ec28e53eb8c162cbe0d9a122b2d751ae
Ben Wintraub <benwintraub@MacBook-Air.local>
2022-12-01 10:47:47 -0800

    added alpaca data api

---
+++ zip.py
@@ -0,0 +1,59 @@
+import alpaca_backtrader_api as alpaca
+import backtrader as bt
+import pytz
+from datetime import datetime
+
+ALPACA_KEY_ID = "PK52ROBY9AUL49LRCBBP"
+ALPACA_SECRET_KEY = "IqlIvevEv2l1rihn6Bg7S8JaJhYsHxMKuOxw4l3k"
+ALPACA_PAPER = True
+
+fromdate = datetime(2020,8,5)
+todate = datetime(2020,8,10)
+
+tickers = ['AAPL']
+timeframes = {
+    '2H':120,
+}
+
+class RSIStack(bt.Strategy):
+
+    def next(self):
+        for i in range(0,len(self.datas)):
+            print(f'{self.datas[i].datetime.datetime(ago=0)} \
+            {self.datas[i].p.dataname}: OHLC: \
+                  o:{self.datas[i].open[0]} \
+                  h:{self.datas[i].high[0]} \
+                  l:{self.datas[i].low[0]} \
+                  c:{self.datas[i].close[0]} \
+                  v:{self.datas[i].volume[0]}')
+
+cerebro = bt.Cerebro()
+cerebro.addstrategy(RSIStack)
+cerebro.broker.setcash(100000)
+cerebro.broker.setcommission(commission=0.0)
+
+store = alpaca.AlpacaStore(
+    key_id=ALPACA_KEY_ID,
+    secret_key=ALPACA_SECRET_KEY,
+    paper=ALPACA_PAPER
+)
+
+DataFactory = store.getdata
+
+for ticker in tickers:
+    for timeframe, minutes in timeframes.items():
+        print(f'Adding ticker {ticker} using {timeframe} timeframe at {minutes} minutes.')
+
+        d = DataFactory(
+            dataname=ticker,
+            timeframe=bt.TimeFrame.Minutes,
+            compression=minutes,
+            fromdate=fromdate,
+            todate=todate,
+            historical=True)
+
+        cerebro.adddata(d)
+
+cerebro.run()
+print("Final Portfolio Value: %.2f" % cerebro.broker.getvalue())
+# cerebro.plot(style='candlestick', barup='green', bardown='red')
