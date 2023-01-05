f6276c2dd6788810e8268d8141f38c75eac2d2dd
Aaditya Rao <Ridgehatsbyaaditya@gmail.com>
2023-01-05 11:30:54 -0800

    Implemented Free Cashflow
    Basic FCF with arbitrary variables

---
+++ FCFE.py
@@ -0,0 +1,12 @@
+# FCFE based on EBIT - (capex - depreciation) - (change in working capital) -(principal payments-debt issuance)
+
+
+capex = 1000
+depreciation = 500
+CIWC = 200
+principalPayments = 100
+debtInsurance = 50
+
+
+FCFE = EBIT - (capex - depreciation) - (CIWC) -(principalPayments-debtIssuance)
+
