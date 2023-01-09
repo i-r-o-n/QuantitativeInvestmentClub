4c5ba7c25d91c0f7cfc84352c837f8022fcb9776
codeitfast <micahkurlandcohen@gmail.com>
2023-01-09 12:46:07 -0800

    Added MatPlotLib funtion and made it object oriented

--- FCFE.py
+++ FCFE.py
@@ -1,12 +1,12 @@
 # FCFE based on EBIT - (capex - depreciation) - (change in working capital) -(principal payments-debt issuance)
 
-
-capex = 1000
-depreciation = 500
-CIWC = 200
-principalPayments = 100
-debtInsurance = 50
-
-
-FCFE = EBIT - (capex - depreciation) - (CIWC) -(principalPayments-debtIssuance)
-
+class Stock:
+    def __init__(self, capex, depreciation, CIWC, principalPayments, debtIssuance, EBIT):
+        #make everything local
+        self.capex = capex #Capital Expenditure
+        self.depreciation = depreciation #Depreciation, possibly total depreciation (can be negative)
+        self.CIWC = CIWC #Change in Working Capital
+        self.principalPayments = principalPayments
+        self.debtIssuance = debtIssuance #TODO: Insurance or Issuance
+        self.EBIT = EBIT #Earnings before interest and taxes
+        self.FCFE = self.EBIT - (self.capex - self.depreciation) - self.CIWC - (self.principalPayments - self.debtIssuance)
---
+++ test.FCFE.py
@@ -0,0 +1,21 @@
+import random
+from FCFE import Stock
+import matplotlib.pyplot as plt
+import numpy as np
+
+capex = 10
+depreciation = .1
+CIWC = 3
+principalPayments = 4
+debtIssuance = 1
+EBIT = 10
+
+x = np.linspace(0, 10, 200)
+y = []
+for i in range(200):
+    y.append(Stock(capex,depreciation,CIWC,principalPayments,debtIssuance,EBIT).FCFE)
+    depreciation += ((random.random() - .5)/10)
+
+fig, ax = plt.subplots()
+ax.plot(x,y)
+plt.show()
