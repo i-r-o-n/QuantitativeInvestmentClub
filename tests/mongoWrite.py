0261b992e8c2c36a8edcf853dff8864563441114
aadityar <aaditya_rao@outlook.com>
2023-02-08 23:15:14 -0800

    write to db logic

---
+++ tests/mongoWrite.py
@@ -0,0 +1,30 @@
+import pymongo
+from bson.objectid import ObjectId
+import datetime
+
+username = 'johnstreetcapital'
+password = 'OJRpUn4mtBxi51Oq'
+client = pymongo.MongoClient(f"mongodb+srv://{username}:{password}@testcluster.yxijioi.mongodb.net/?retryWrites=true&w=majority")
+db = client['companyBalanceSheet']
+companies = db['companies']
+
+
+def mongoPost(post):
+    postID = companies.insert_one(post).inserted_id
+    print(postID)
+    return postID
+
+def mongoQuery(query):
+    print(companies.find_one({"_id": ObjectId(query)}))
+
+def main():
+
+    post = {"name": "microsoft",
+            "industry": ['technology', 'consumer electronics'],
+            "cashflow": 700000,
+            "lastUpdate": datetime.datetime.utcnow()}
+
+    mongoQuery(mongoPost(post))
+
+if __name__ == '__main__':
+    main()
