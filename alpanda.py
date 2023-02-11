3220e913c8b83ec3fe3373a349b605d32f2ae487
Ben-Wintraub24 <benw24@downtownschoolseattle.org>
2023-02-10 23:07:59 -0800

    Alpanda.py v1

--- alpanda.py
+++ alpanda.py
@@ -1,59 +1,15 @@
-import alpaca_backtrader_api as alpaca
-import backtrader as bt
-from datetime import datetime
+import alpaca_trade_api as api
+from alpaca_trade_api.rest import TimeFrame
 from alpaca_account import paper_account
 
 ALPACA_KEY_ID = paper_account['api_key']
 ALPACA_SECRET_KEY = paper_account['api_secret']
 ALPACA_PAPER = True
 
-fromdate = datetime(2020,8,5)
-todate = datetime(2020,8,10)
+alpaca = api.REST(ALPACA_KEY_ID, ALPACA_SECRET_KEY)
 
-tickers = ['AAPL', 'MSFT']
-timeframes = {
-    '2H':120,
-    '5H':300
-}
+ticker = 'AAPL'
 
-prices = dict()
+prices = alpaca.get_bars(ticker, TimeFrame.Day, start="2022-02-09T00:00:00-08:00", end=None,limit=365)
 
-class RSIStack(bt.Strategy):
-
-    def next(self):
-        for i in range(0,len(self.datas)):
-            date = self.datas[i].datetime.datetime(ago=0)
-            price = self.datas[i].close[0]
-            prices[date] = price
-            
-
-cerebro = bt.Cerebro()
-cerebro.addstrategy(RSIStack)
-# cerebro.broker.setcash(100000)
-# cerebro.broker.setcommission(commission=0.0)
-
-store = alpaca.AlpacaStore(
-    key_id=ALPACA_KEY_ID,
-    secret_key=ALPACA_SECRET_KEY,
-    paper=ALPACA_PAPER
-)
-
-DataFactory = store.getdata
-
-for ticker in tickers:
-    for timeframe, minutes in timeframes.items():
-        print(f'Adding ticker {ticker} using {timeframe} timeframe at {minutes} minutes.')
-
-        d = DataFactory(
-            dataname=ticker,
-            timeframe=bt.TimeFrame.Minutes,
-            compression=minutes,
-            fromdate=fromdate,
-            todate=todate,
-            historical=True)
-
-        cerebro.adddata(d)
-
-cerebro.run()
-print("Final Portfolio Value: %.2f" % cerebro.broker.getvalue())
-# cerebro.plot(style='candlestick', barup='green', bardown='red')
+print(prices.df['close'])
