efbfb8aa3ddf0f93f50a288f91e8ee09b94eb708
aadityar <aaditya_rao@outlook.com>
2023-02-10 17:44:46 -0800

    Created MongoPost and MongoQuery Functions

--- databaseFunctions.py
+++ databaseFunctions.py
@@ -14,9 +14,19 @@ collection = ''
 def mongoPost(post):
     '''
     This function takes a dictionary as an argument and inserts it into the database.
-    
+    Post strucutre:
+   
     '''
     client = pymongo.MongoClient(f"mongodb+srv://{username}:{password}@testcluster.yxijioi.mongodb.net/?retryWrites=true&w=majority")
     db = client[database]
     collection = db[collection]
     collection.insert_one(post)
+
+def mongoQuery(query):
+    '''
+    This function takes a string as an argument and returns the document with the corresponding ID.
+    '''
+    client = pymongo.MongoClient(f"mongodb+srv://{username}:{password}@testcluster.yxijioi.mongodb.net/?retryWrites=true&w=majority")
+    db = client[database]
+    collection = db[collection]
+    return collection.find_one({"name": query})
