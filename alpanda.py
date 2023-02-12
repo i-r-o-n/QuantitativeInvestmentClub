34cdcf4b725b7f91b3d8f0fb610940b3e04382f6
Ben-Wintraub24 <benw24@downtownschoolseattle.org>
2023-02-12 00:29:59 -0800

    Saved dataframe to csv

--- alpanda.py
+++ alpanda.py
@@ -1,15 +1,25 @@
-import alpaca_trade_api as api
-from alpaca_trade_api.rest import TimeFrame
+from alpaca_trade_api.rest import TimeFrame, REST
 from alpaca_account import paper_account
+import datetime as dt
+import pandas as pd
 
 ALPACA_KEY_ID = paper_account['api_key']
 ALPACA_SECRET_KEY = paper_account['api_secret']
 ALPACA_PAPER = True
 
-alpaca = api.REST(ALPACA_KEY_ID, ALPACA_SECRET_KEY)
+alpaca = REST(ALPACA_KEY_ID, ALPACA_SECRET_KEY)
 
 ticker = 'AAPL'
 
-prices = alpaca.get_bars(ticker, TimeFrame.Day, start="2022-02-09T00:00:00-08:00", end=None,limit=365)
+def price_from_start_date(start: str) -> pd.DataFrame:
 
-print(prices.df['close'])
+	end = dt.datetime.now(dt.timezone.utc) - dt.timedelta(minutes=15)
+	prices = alpaca.get_bars(ticker, TimeFrame.Day, start=start.isoformat(), end=end.isoformat()).df
+
+	return prices
+
+PST = dt.timezone(dt.timedelta(hours=-8))
+last_year = dt.datetime(2022, 2, 11, tzinfo=PST)
+
+# price_from_start_date(last_year)['close'].to_csv('AAPL_closing_price.csv')
+print(pd.read_csv('AAPL_closing_price.csv').head())
