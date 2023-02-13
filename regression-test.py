73cc6960b145f63144d8d348c76444f86d393b00
Ben-Wintraub24 <benw24@downtownschoolseattle.org>
2023-02-13 09:31:43 -0800

    Use real stock data

--- regression-test.py
+++ regression-test.py
@@ -2,20 +2,27 @@ import random
 from Archive.FCFE import Stock
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
+import matplotlib.ticker as mtick
 
-capex = 10
-depreciation = .1
-CIWC = 3
-principalPayments = 4
-debtIssuance = 1
-EBIT = 10
+# capex = 10
+# depreciation = .1
+# CIWC = 3
+# principalPayments = 4
+# debtIssuance = 1
+# EBIT = 10
+
+#Read from csv + 
+df1 = pd.read_csv('AAPL_closing_price.csv')
+df1['timestamp'] = pd.to_datetime(df1['timestamp'], format="%Y-%m-%d %H:%M:%S%z")
+y = df1['close']
 
 #make fake data
-x = np.linspace(0, 10, 200)
-y = []
-for i in range(200):
-    y.append(Stock(capex,depreciation,CIWC,principalPayments,debtIssuance,EBIT).FCFE)
-    depreciation += ((random.random() - .5))
+x = np.linspace(0, 1, len(df1['timestamp']))
+# y = []
+# for i in range(200):
+#     y.append(Stock(capex,depreciation,CIWC,principalPayments,debtIssuance,EBIT).FCFE)
+#     depreciation += ((random.random() - .5))
 
 def smooth(n, y):
     #n = distance to blur/smooth, y is an array of the original points
@@ -35,7 +42,16 @@ def smooth(n, y):
 
 
 fig, ax = plt.subplots()
-ax.plot(x,y, c="#151515")
-ax.plot(x,smooth(3, y), c="r")
-ax.plot(x,smooth(10, y), c="g")
+
+ax.plot(df1['timestamp'],y)
+ax.plot(df1['timestamp'],smooth(10, y))
+
+plt.style.use('ggplot') #use pretty colors
+plt.xticks(rotation=45, ha="right", rotation_mode="anchor") #rotate the x-axis values
+ax.yaxis.set_major_formatter(mtick.FormatStrFormatter("$%.0f")) #add dollar sign
+plt.subplots_adjust(bottom = 0.2, top = 0.9) #ensure dates fit on the screen
+
+plt.ylabel('Stock Price')
+plt.xlabel('Dates')
+
 plt.show()
